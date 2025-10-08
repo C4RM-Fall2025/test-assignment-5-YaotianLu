719f640dc884c865a35e9e663a8ec767748401e3
YaotianLu <yl5856@columbia.edu>
2025-10-07 20:53:13 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    n = m * ppy                    
+    r = y / ppy                    
+    c = face * couponRate / ppy    
+
+    t = np.arange(1, n + 1)        
+    pvcoupons = np.sum(c / (1 + r) ** t)  
+    pvface = face / (1 + r) ** n         
+
+    return pvcoupons + pvface
+
